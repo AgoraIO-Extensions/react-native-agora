689c0638779ca69a209089b2ccd2e3c427c73d95
薛鹏翔 <me@xuepx.cn>
2017-11-20 14:28:45 +0800

    Update AgoraView.js
    更新适应RN0.40+

--- AgoraView.js
+++ AgoraView.js
@@ -2,7 +2,8 @@
  * Created by DB on 2017/6/23.
  */
 
-import  React, {Component, PropTypes} from 'react'
+import  React, {Component} from 'react'
+import PropTypes from 'prop-types'
 import {
     requireNativeComponent,
     View,
