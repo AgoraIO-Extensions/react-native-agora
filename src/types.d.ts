0ecc2062b7a1ec7bb6716b24660276bcae4a00d5
matrixbirds <yooobuntu@gmail.com>
2019-01-30 15:54:39 +0800

    stash

--- src/types.d.ts
+++ src/types.d.ts
@@ -2,22 +2,23 @@ import { View, ViewProps } from 'react-native';
 
 declare module "IAgora";
 
-export interface IVideoEncoderConfig {
+export interface VideoEncoderConfig {
   width: number,
   height: number,
   bitrate: number,
   frameRate: number,
   orientationMode: number,
-};
-export interface IOption {
+}
+
+export interface Option {
   appid: String,
   videoProfile: number
   channelProfile: number,
-  videoEncoderConfig: IVideoEncoderConfig,
+  videoEncoderConfig: VideoEncoderConfig,
   clientRole: number
 }
 
-export interface IPublisherConfig {
+export interface PublisherConfig {
   width: number,
   height: number,
   framerate: number,
@@ -30,7 +31,7 @@ export interface IPublisherConfig {
   owner: boolean
 }
 
-export interface IBackgroundImage {
+export interface BackgroundImage {
   url: string,
   x: number,
   y: number,
@@ -38,57 +39,57 @@ export interface IBackgroundImage {
   height: number
 }
 
-export interface IRect {
+export interface Rect {
   x: number,
   y: number,
   width: number,
   height: number
-};
+}
 
-export interface ISize {
+export interface Size {
   width: number,
   height: number
-};
+}
 
-export interface ITranscodingUser {
+export interface TranscodingUser {
   uid: number,
   zOrder: number,
   rect: Rect,
   alpha: number,
   audioChannel: number
-};
+}
 
-export interface IColor {
+export interface Color {
   red: number,
   green: number,
   blue: number,
   alpha: number
-};
+}
 
-export interface ILiveTranscoding {
-  size: ISize,
+export interface LiveTranscoding {
+  size: Size,
   videoBitrate: number,
   videoFramerate: number,
   lowLatency: boolean,
   videoGop: number,
   videoCodecProfile: number,
-  transcodingUsers: Array<ITranscodingUser>,
+  transcodingUsers: Array<TranscodingUser>,
   transcodingExtraInfo: string,
-  watermark: IBackgroundImage,
-  backgroundImage: IBackgroundImage,
-  backgroundColor: IColor,
+  watermark: BackgroundImage,
+  backgroundImage: BackgroundImage,
+  backgroundColor: Color,
   audioSampleRate: number,
   audioBitrate: number,
   audioChannels: number,
-};
+}
 
-export interface IVideoOption {
+export interface VideoOption {
   uid: Number,
   renderMode: number,
   reactTag: number
 }
 
-export interface IEventScheduler {
+export interface EventScheduler {
   onFirstRemoteVideoDecoded: Function,
   onJoinChannelSuccess: Function,
   onUserOffline: Function,
@@ -101,15 +102,16 @@ export interface IEventScheduler {
   onStreamMessageError: Function
 }
 
-export type ICallback<T> = (err, data) => T;
+export type Callback<T> = (err: any, data: any) => T;
 
 export type Nullable<T> = T | null | undefined;
 
 export type String = Nullable<string>;
 export type Number = Nullable<number> | 0;
 
-export interface IAgoraViewProps extends ViewProps {
+export interface AgoraViewProps extends ViewProps {
   showLocalVideo: boolean,
   remoteUid: Number,
-  zOrderMediaOverlay: boolean
+  zOrderMediaOverlay: boolean,
+  [key:string]: any;
 }
