8152ced9b15040dd2ee41921246bf8cef89e6317
matrixbirds <yooobuntu@gmail.com>
2019-02-02 00:27:58 +0800

    add AgoraRtcCryptoLoader

---
+++ ios/RCTAgora/libs/AgoraRtcCryptoLoader.framework/Headers/AgoraRtcCryptoLoader.h
@@ -0,0 +1,12 @@
+//
+//  AgoraRtcCryptoLoader.h
+//  AgoraRtcCryptoLoader
+//
+//  Copyright © 2018 Agora. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AgoraRtcCryptoLoader : NSObject
+
+@end
