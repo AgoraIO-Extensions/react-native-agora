5d65b41ed86fa43f2c7e30014037eaae386ce280
matrixbirds <yooobuntu@gmail.com>
2019-02-01 20:41:45 +0800

    remove unused code

--- samples/simpleDemo/App.js
+++ samples/simpleDemo/App.js
@@ -8,16 +8,9 @@
 
 import React, {Component} from 'react';
 import {
-  Platform, StyleSheet, Text, View, TouchableOpacity, TextInput
+  StyleSheet, Text, View, TouchableOpacity, TextInput
 } from 'react-native';
 import AgoraRTCView from './components/agora';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
 const styles = StyleSheet.create({
   container: {
