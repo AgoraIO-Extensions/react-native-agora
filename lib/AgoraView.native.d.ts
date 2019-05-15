294268eac886e729ad0aa48c4c9ed1f766063d47
matrixbirds <yooobuntu@gmail.com>
2019-05-15 11:39:31 +0800

    feat. support typescript typing

---
+++ lib/AgoraView.native.d.ts
@@ -0,0 +1,23 @@
+import React from 'react';
+import { AgoraViewProps } from "./types.d";
+/**
+ * AgoraView is the render layer for rendering video stream
+ *
+ * This class is used to rendering native sdk stream
+ *
+ * @props {@link AgoraViewProps}
+ */
+export default class AgoraView extends React.Component<AgoraViewProps> {
+    /**
+     * render
+     *
+     * It would render view for VideoStream
+     */
+    render(): JSX.Element;
+    /**
+     * getHTMLProps
+     *
+     * get agora view props
+     */
+    private getHTMLProps;
+}
