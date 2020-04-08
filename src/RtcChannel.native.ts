5eed59b0801d2ea37a416f2b07d192bf2a7ee081
HUI <15215604969@163.com>
2020-04-08 21:51:41 +0800

    git ignore lib

--- src/RtcChannel.native.ts
+++ src/RtcChannel.native.ts
@@ -48,6 +48,7 @@ export default class RtcChannel implements RtcAudioInterface, RtcVideoInterface,
     }
 
     destroy(): Promise<void> {
+        this.removeAllListeners();
         RtcChannel.channels.delete(this._channelId);
         return RtcChannelModule.destroy(this._channelId)
     }
