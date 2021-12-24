3525f2c8ff5fb6574e21b94588f8a951e109c639
Arthas <luxuhui@agora.io>
2021-12-24 16:09:22 +0800

    fix: enable to enabled

--- src/common/RtcEngine.native.ts
+++ src/common/RtcEngine.native.ts
@@ -1335,14 +1335,14 @@ export default class RtcEngine implements RtcEngineInterface {
    *   - iPad Air (4th generation)
    *
    * @param uid The user ID of the remote user.
-   * @param enable Determines whether to enable super resolution for the remote user's video:
+   * @param enabled Determines whether to enable super resolution for the remote user's video:
    *   - `true`: Enable super resolution.
    *   - `false`: Do not enable super resolution.
    */
-  enableRemoteSuperResolution(uid: number, enable: boolean): Promise<void> {
+  enableRemoteSuperResolution(uid: number, enabled: boolean): Promise<void> {
     return RtcEngine._callMethod('enableRemoteSuperResolution', {
       uid,
-      enable,
+      enabled,
     });
   }
 
