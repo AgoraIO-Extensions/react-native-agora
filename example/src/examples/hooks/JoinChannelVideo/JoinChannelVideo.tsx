f83ab999735b1206a392b33fee815bc3e30df479
luz33c <33cluz@gmail.com>
2022-09-21 11:52:33 +0800

    feat: add comment

--- example/src/examples/hooks/JoinChannelVideo/JoinChannelVideo.tsx
+++ example/src/examples/hooks/JoinChannelVideo/JoinChannelVideo.tsx
@@ -66,6 +66,9 @@ export default function JoinChannelVideo(props: {} & StackScreenProps<{}>) {
     VideoViewSetupMode.VideoViewSetupReplace
   );
 
+  /**
+   * Step 1: initRtcEngine
+   */
   const initRtcEngine = async () => {
     if (!appId) {
       console.log(`appId is invalid`);
@@ -84,16 +87,18 @@ export default function JoinChannelVideo(props: {} & StackScreenProps<{}>) {
         PermissionsAndroid.PERMISSIONS.CAMERA,
       ]);
     }
-
+    // Need to enable video on this case
+    // If you only call `enableAudio`, only relay the audio stream to the target channel
     engine.enableVideo();
+
+    // Start preview before joinChannel
     engine.startPreview();
     setStartPreview(true);
   };
 
-  const leaveChannel = () => {
-    engine.leaveChannel();
-  };
-
+  /**
+   * Step 2: joinChannel
+   */
   const joinChannel = () => {
     if (!channelId) {
       console.error('channelId is invalid');
@@ -103,9 +108,32 @@ export default function JoinChannelVideo(props: {} & StackScreenProps<{}>) {
       console.error('uid is invalid');
       return;
     }
+
+    // start joining channel
+    // 1. Users can only see each other after they join the
+    // same channel successfully using the same app id.
+    // 2. If app certificate is turned on at dashboard, token is needed
+    // when joining channel. The channel name and uid used to calculate
+    // the token has to match the ones used for channel join
     engine.joinChannel(token, channelId, uid, {
+      // Make myself as the broadcaster to send stream to remote
       clientRoleType: ClientRoleType.ClientRoleBroadcaster,
     });
+  };
+
+  /**
+   * Step 3 (Optional): switchCamera
+   */
+  const updateSwitchCamera = () => {
+    engine?.switchCamera();
+    setSwitchCamera(!switchCamera);
+  };
+
+  /**
+   * Step 4: leaveChannel
+   */
+  const leaveChannel = () => {
+    engine.leaveChannel();
   };
 
   useEffect(() => {
@@ -344,8 +372,7 @@ export default function JoinChannelVideo(props: {} & StackScreenProps<{}>) {
           disabled={!startPreview && !joinChannelSuccess}
           title={`switchCamera`}
           onPress={() => {
-            engine.switchCamera();
-            setSwitchCamera(!switchCamera);
+            updateSwitchCamera();
           }}
         />
       </>
