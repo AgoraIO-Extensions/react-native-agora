a174a8843827bc62f0547030bc640ecdbffeba04
liang <62172559+luz33c@users.noreply.github.com>
2022-09-22 14:54:03 +0800

    feat: addListener case (#524)
    * feat: addListener case
    
    * feat: add comment

---
+++ example/src/examples/hooks/index.ts
@@ -0,0 +1,12 @@
+import JoinChannelVideo from './JoinChannelVideo/JoinChannelVideo';
+
+const Hooks = {
+  title: 'Hooks',
+  data: [
+    {
+      name: 'JoinChannelVideoHooks',
+      component: JoinChannelVideo,
+    },
+  ],
+};
+export default Hooks;
