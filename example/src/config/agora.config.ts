783003bca66da015965329268fd1de1f53205b6d
pr <pedrocfyd@gmail.com>
2023-01-03 16:10:58 -0600

    refactor: migrate JoinChannelAudio Example from class to hook

--- example/src/config/agora.config.ts
+++ example/src/config/agora.config.ts
@@ -1,8 +1,8 @@
 const config = {
   // Get your own App ID at https://dashboard.agora.io/
-  appId: '',
+  appId: 'fcfce76b866c49ad92d4e457a7212e21',
   // Please refer to https://docs.agora.io/en/Agora%20Platform/token
-  token: '',
+  token: '007eJxTYDh0Qd1Tum5HfcjjWen68/+z8W92KmlbcP/RrY7/u8NvfAxVYEhLTktONTdLsjAzSzaxTEyxNEoxSTUxNU80NzI0SjUyvBu1JbkhkJFBYOcUVkYGCATxmRlyKjIYGADk5SGx',
   channelId: 'lxh',
   uid: 0,
 };
