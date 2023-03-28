889bf301ddba1f28835ef29237e0425265189686
guoxianzhe <53285945+guoxianzhe@users.noreply.github.com>
2023-03-28 17:27:53 +0900

    refactor: migrate StringUid Example from class to hook (#610)
    refactor: migrate JoinMultipleChannel Example from class to hook
    refactor: migrate VirtualBackground Example from class to hook
    feat: add hook useInitRtcEngine
    docs: example readme updated

---
+++ example/src/utils/log.ts
@@ -0,0 +1,39 @@
+import { Alert } from 'react-native';
+
+export const _logSink = (
+  level: 'debug' | 'log' | 'info' | 'warn' | 'error',
+  message?: any,
+  ...optionalParams: any[]
+): string => {
+  if (level === 'error' && !__DEV__) {
+    alert(message);
+  } else {
+    console[level](message, ...optionalParams);
+  }
+  const content = `${optionalParams.map((v) => JSON.stringify(v))}`;
+  return content;
+};
+
+export const alert = (title: string, message?: string): void => {
+  Alert.alert(title, message);
+};
+
+export const debug = (message?: any, ...optionalParams: any[]): void => {
+  alert(message, _logSink('debug', message, optionalParams));
+};
+
+export const log = (message?: any, ...optionalParams: any[]): void => {
+  _logSink('log', message, optionalParams);
+};
+
+export const info = (message?: any, ...optionalParams: any[]): void => {
+  _logSink('info', message, optionalParams);
+};
+
+export const warn = (message?: any, ...optionalParams: any[]): void => {
+  _logSink('warn', message, optionalParams);
+};
+
+export const error = (message?: any, ...optionalParams: any[]): void => {
+  _logSink('error', message, optionalParams);
+};
