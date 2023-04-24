e5178975e7c064ba448f46328ce0ce34c87b6efe
HUI <luxuhui@agora.io>
2023-04-24 10:05:40 +0800

    chore: optimize

--- example/src/config/agora.config.ts
+++ example/src/config/agora.config.ts
@@ -7,7 +7,7 @@ try {
 
 const config = {
   // Get your own App ID at https://dashboard.agora.io/
-  appId: localAppId,
+  appId: `${localAppId}`,
   // Please refer to https://docs.agora.io/en/Agora%20Platform/token
   token: '',
   channelId: 'testdcg',
