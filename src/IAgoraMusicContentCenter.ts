24fde7554d4c902b3c0da9d98bb020c9873512d4
Arthas <luxuhui@agora.io>
2023-05-23 10:38:50 +0800

    [AUTO] Generate comments by iris-doc (#657)
    Co-authored-by: Nero-Hu <Nero-Hu@users.noreply.github.com>

--- src/IAgoraMusicContentCenter.ts
+++ src/IAgoraMusicContentCenter.ts
@@ -377,19 +377,12 @@ export abstract class IMusicContentCenter {
   abstract preload(songCode: number, jsonOption?: string): number;
 
   /**
-   * 删除已缓存的音乐资源。
-   * 你可以调用该方法删除某一已缓存的音乐资源，如需删除多个音乐资源，你可以多次调用该方法。 The cached media file currently being played will not be deleted.
-   *
-   * @param songCode 待删除的音乐资源的编号。
-   *
-   * @returns
-   * 0: 方法调用成功，音乐资源已删除。< 0: Failure.
+   * @ignore
    */
   abstract removeCache(songCode: number): number;
 
   /**
-   * 获取已缓存的音乐资源信息。
-   * 当你不再需要使用已缓存的音乐资源时，你需要及时释放内存以防止内存泄漏。
+   * @ignore
    */
   abstract getCaches(): { cacheInfo: MusicCacheInfo[]; cacheInfoSize: number };
 
