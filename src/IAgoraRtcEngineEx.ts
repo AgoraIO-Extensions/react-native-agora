99d7b28da4e11be617764db04f7da3bccd860c91
Nero-Hu <Nero-Hu@users.noreply.github.com>
2023-10-07 03:41:34 +0000

    [AUTO] Generate comments by iris-doc

--- src/IAgoraRtcEngineEx.ts
+++ src/IAgoraRtcEngineEx.ts
@@ -901,7 +901,7 @@ export abstract class IRtcEngineEx extends IRtcEngine {
    * This method can take screenshots for multiple video streams and upload them. When video screenshot and upload function is enabled, the SDK takes screenshots and uploads videos sent by local users based on the type and frequency of the module you set in ContentInspectConfig. After video screenshot and upload, the Agora server sends the callback notification to your app server in HTTPS requests and sends all screenshots to the third-party cloud storage service. Before calling this method, ensure that you have contacted to activate the video screenshot upload service.
    *
    * @param enabled Whether to enable video screenshot and upload : true : Enables video screenshot and upload. false : Disables video screenshot and upload.
-   * @param config Configuration of video screenshot and upload. See ContentInspectConfig. When the video moderation module is set to video moderation via Agora self-developed extension(ContentInspectSupervision ContentInspectImageModeration), the video screenshot and upload dynamic library libagora_content_inspect_extension.dll is required. Deleting this library disables the screenshot and upload feature.
+   * @param config Configuration of video screenshot and upload. See ContentInspectConfig. When the video moderation module is set to video moderation via Agora self-developed extension(ContentInspectSupervision), the video screenshot and upload dynamic library libagora_content_inspect_extension.dll is required. Deleting this library disables the screenshot and upload feature.
    * @param connection The connection information. See RtcConnection.
    *
    * @returns
