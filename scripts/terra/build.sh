86307b7a96e2f0c5019bf076a6a3b125b7914268
gxz <guoxianzhe@agora.io>
2023-11-27 14:40:04 +0800

    chore: ci

--- scripts/terra/build.sh
+++ scripts/terra/build.sh
@@ -5,13 +5,9 @@ set -x
 MY_PATH=$(realpath $(dirname "$0"))
 PROJECT_ROOT=$(realpath ${MY_PATH}/../..)
 
-pushd ${MY_PATH}
-
 npm exec terra -- run \
     --config ${PROJECT_ROOT}/scripts/terra/terra_config_main.yaml  \
     --output-dir=${PROJECT_ROOT}/lib/src
-
-popd
 
 cd ${PROJECT_ROOT}
 
