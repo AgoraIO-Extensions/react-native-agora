1de1072e3ce1e5a0772dde75e40075024d34dbf3
gxz <guoxianzhe@agora.io>
2024-01-12 10:35:09 +0800

    chore: optimize

--- src/internal/AgoraMediaBaseInternal.ts
+++ src/internal/AgoraMediaBaseInternal.ts
@@ -2,14 +2,14 @@ import { IVideoFrameMetaInfo, MetaInfoKey } from '../AgoraMediaBase';
 import { IVideoFrameMetaInfoImpl } from '../impl/AgoraMediaBaseImpl';
 
 export class VideoFrameMetaInfoInternal extends IVideoFrameMetaInfoImpl {
-  private _VideoFrameMetaInfo: IVideoFrameMetaInfo | undefined;
+  private _videoFrameMetaInfo: IVideoFrameMetaInfo | undefined;
   constructor(videoFrameMetaInfo: IVideoFrameMetaInfo | undefined) {
     super();
-    this._VideoFrameMetaInfo = videoFrameMetaInfo;
+    this._videoFrameMetaInfo = videoFrameMetaInfo;
   }
 
   override getMetaInfoStr(key: MetaInfoKey): string {
     // @ts-ignore
-    return this._VideoFrameMetaInfo?.[key];
+    return this._videoFrameMetaInfo?.[key];
   }
 }
