97b43bc39fdcf20753d4b6f375a6d2f870dd26de
gxz <guoxianzhe@agora.io>
2024-01-30 14:21:42 +0800

    chore: optimize

--- scripts/terra/generate-prepare.sh
+++ scripts/terra/generate-prepare.sh
@@ -2,6 +2,9 @@
 set -e
 set -x
 
+# treated as a completely separate project (not even a workspace), create an empty yarn.lock file in it.
+touch yarn.lock
 rm -rf node_modules
 rm -rf .terra
-yarn
+yarn install
+rm yarn.lock
