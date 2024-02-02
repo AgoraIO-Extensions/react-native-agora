10ac94d50739f0bb61ddfdd31d1308e6388ff7b5
gxz <guoxianzhe@agora.io>
2024-02-02 13:17:54 +0800

    feat: upgrade react-native to 0.73.2

--- scripts/bootstrap.cjs
+++ scripts/bootstrap.cjs
@@ -17,25 +17,6 @@ module.exports = {
           if (process.env.PWD.includes('scripts/terra')) {
             return;
           }
-          if (
-            child_process.spawnSync('yarn', ['patch-package'], {
-              stdio: 'inherit',
-              encoding: 'utf-8',
-              shell: true,
-            }).status !== 0
-          ) {
-            throw new Error('Failed to run patch-package');
-          }
-
-          if (
-            child_process.spawnSync('yarn', ['example', 'patch-package'], {
-              stdio: 'inherit',
-              encoding: 'utf-8',
-              shell: true,
-            }).status !== 0
-          ) {
-            throw new Error('Failed to run patch-package at example');
-          }
 
           if (
             child_process.spawnSync('yarn', ['build:ts-interface'], {
