edf88fe332c2dd5f69e863f2c1edd2286aeae25c
gxz <guoxianzhe@agora.io>
2024-03-21 19:25:44 +0800

    chore: optimize

--- scripts/mirror.sh
+++ scripts/mirror.sh
@@ -11,5 +11,9 @@ $LINE
 " ${PROJECT_ROOT}/.yarnrc.yml
 fi
 
+echo ".yarnrc.yml updated"
+
 sed -i "" 's#"react-native-agora-rawdata": "github:AgoraLibrary/react-native-agora-rawdata"#"react-native-agora-rawdata": "git+https://gitee.com/agoraio-community/react-native-agora-rawdata.git"#g' ${PROJECT_ROOT}/example/package.json
 sed -i "" 's#"react-native-image-tool": "github:LichKing-2234/react-native-image-tools"#"react-native-image-tool": "git+https://gitee.com/agoraio-community/react-native-image-tools.git"#g' ${PROJECT_ROOT}/example/package.json
+
+echo "example/package.json updated"
