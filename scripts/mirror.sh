ecf1a6fd884bc2499b6842184759d8c7178f4001
gxz <guoxianzhe@agora.io>
2024-03-21 14:55:01 +0800

    chore: optimize

--- scripts/mirror.sh
+++ scripts/mirror.sh
@@ -4,7 +4,12 @@ set -x
 MY_PATH=$(realpath $(dirname "$0"))
 PROJECT_ROOT=$(realpath ${MY_PATH}/..)
 
-sed -i "" '1i\
-npmRegistryServer: https://registry.npmmirror.com
-' ${PROJECT_ROOT}/.yarnrc.yml
+LINE="npmRegistryServer: https://registry.npmmirror.com"
+if ! grep -q "$LINE" ${PROJECT_ROOT}/.yarnrc.yml; then
+  sed -i "" "1i\\
+$LINE
+" ${PROJECT_ROOT}/.yarnrc.yml
+fi
 
+sed -i "" 's#"react-native-agora-rawdata": "github:AgoraLibrary/react-native-agora-rawdata"#"react-native-agora-rawdata": "git+https://gitee.com/agoraio-community/react-native-agora-rawdata.git"#g' ${PROJECT_ROOT}/example/package.json
+sed -i "" 's#"react-native-image-tool": "github:LichKing-2234/react-native-image-tools"#"react-native-image-tool": "git+https://gitee.com/agoraio-community/react-native-image-tools.git"#g' ${PROJECT_ROOT}/example/package.json
