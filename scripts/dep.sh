5fac81f3de43f7dd0bd7652a2ae558a9440712f1
gxz <guoxianzhe@agora.io>
2025-02-07 15:20:19 +0800

    chore: optimize

--- scripts/dep.sh
+++ scripts/dep.sh
@@ -1,50 +1,44 @@
 #!/bin/bash
 set -e
-set -x
+set +x
 MY_PATH=$(realpath $(dirname "$0"))
 PROJECT_ROOT=$(realpath ${MY_PATH}/..)
 ANDROID_BUILD_GRADLE_PATH="${PROJECT_ROOT}/android/build.gradle"
-# Check if INPUT is provided
+iOS_PODSPEC_PATH="${PROJECT_ROOT}/react-native-agora.podspec"
+EXAMPLE_IOS_PODFILE_PATH="${PROJECT_ROOT}/example/ios/Podfile"
 if [ "$#" -lt 1 ]; then
     exit 1
 fi
 INPUT=$1
 
-echo $INPUT
+MAVEN_DEPENDENCIES=$(echo "$INPUT" | jq -r '.[] | select(.platform == "Android") | .maven[]')
+IRIS_MAVEN_DEPENDENCIES=$(echo "$INPUT" | jq -r '.[] | select(.platform == "Android") | .iris_maven[]')
 
-MAVEN_DEPENDENCIES=$(echo "$INPUT" | jq -r '.[] | select(.platform == "Android") | .maven[] | .[]')
-IRIS_MAVEN_DEPENDENCIES=$(echo "$INPUT" | jq -r '.[] | select(.platform == "Android") | .iris_maven[] | .[]')
+if [ -z "$MAVEN_DEPENDENCIES" ] && [ -z "$IRIS_MAVEN_DEPENDENCIES" ]; then
+  echo "No Android maven dependencies need to change."
+  exit 0
+else
+  ALL_DEPENDENCIES=$(printf "%s\n%s" "$MAVEN_DEPENDENCIES" "$IRIS_MAVEN_DEPENDENCIES" | sed 's/^/  /')
+  sed -i.bak -e '/\/\/\/ dependencies start/,/\/\/\/ dependencies end/{//!d;}' -e "/\/\/\/ dependencies start/r /dev/stdin" "$ANDROID_BUILD_GRADLE_PATH" <<< "$ALL_DEPENDENCIES"
+  rm "${ANDROID_BUILD_GRADLE_PATH}.bak"
+  echo "android/build.gradle updated."
+fi
 
-# Combine dependencies into a single array
-DEPENDENCIES=($MAVEN_DEPENDENCIES $IRIS_MAVEN_DEPENDENCIES)
+COCOAPODS_DEPENDENCIES=$(echo "$INPUT" | jq -r '.[] | select(.platform == "iOS") | .cocoapods[]')
+IRIS_COCOAPODS_DEPENDENCIES=$(echo "$INPUT" | jq -r '.[] | select(.platform == "iOS") | .iris_cocoapods[]')
 
-# Use awk to clear existing dependencies and insert new ones
-awk -v deps="${DEPENDENCIES[*]}" '
-    BEGIN {
-        split(deps, dep_array, " ")
-        in_deps_block = 0
-    }
-    /\/\/\/ dependencies start/ {
-        print
-        for (i in dep_array) {
-            if (dep_array[i] != "") {
-                print dep_array[i]
-            }
-        }
-        in_deps_block = 1
-        next
-    }
-    /\/\/\/ dependencies end/ {
-        print
-        in_deps_block = 0
-        next
-    }
-    {
-        if (!in_deps_block) {
-            print
-        }
-    }
-' "$ANDROID_BUILD_GRADLE_PATH" > "$ANDROID_BUILD_GRADLE_PATH.tmp"
+if [ -z "$COCOAPODS_DEPENDENCIES" ] && [ -z "$IRIS_COCOAPODS_DEPENDENCIES" ]; then
+  echo "No iOS cocoapods dependencies need to change."
+  exit 0
+else
+  ALL_DEPENDENCIES=$(printf "%s\n%s" "$COCOAPODS_DEPENDENCIES" "$IRIS_COCOAPODS_DEPENDENCIES" | sed 's/^pod /s.dependency /' | sed 's/^/  /')
+  # ALL_DEPENDENCIES=$(printf "%s\n%s" "$COCOAPODS_DEPENDENCIES" "$IRIS_COCOAPODS_DEPENDENCIES" | sed 's/^/  /' | sed "s/^pod /s.dependency /")
+  sed -i.bak -e '/#dependencies start/,/#dependencies end/{//!d;}' -e "/#dependencies start/r /dev/stdin" "$iOS_PODSPEC_PATH" <<< "$ALL_DEPENDENCIES"
+  rm "${iOS_PODSPEC_PATH}.bak"
+  echo "react-native-agora.podspec updated."
 
-# Move the temporary file back to the original file
-mv "$ANDROID_BUILD_GRADLE_PATH.tmp" "$ANDROID_BUILD_GRADLE_PATH"
+  EXAMPLE_DEPENDENCIES=$(printf "%s\n%s" "$COCOAPODS_DEPENDENCIES" | sed 's/^/  /')
+  sed -i.bak -e '/#dependencies start/,/#dependencies end/{//!d;}' -e "/#dependencies start/r /dev/stdin" "$EXAMPLE_IOS_PODFILE_PATH" <<< "$EXAMPLE_DEPENDENCIES"
+  rm "${EXAMPLE_IOS_PODFILE_PATH}.bak"
+  echo "example/ios/Podfile updated."
+fi
