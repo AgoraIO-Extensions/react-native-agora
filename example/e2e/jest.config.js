f1ec9bb8134cfc63bb75e49ece0066f4881594d2
guoxianzhe <53285945+guoxianzhe@users.noreply.github.com>
2025-02-27 21:56:36 +0800

    chore: optimize

--- example/e2e/jest.config.js
+++ example/e2e/jest.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   rootDir: '..',
   testMatch: ['<rootDir>/e2e/**/*.test.js'],
-  testTimeout: 120000,
+  testTimeout: 6000000,
   maxWorkers: 1,
   globalSetup: 'detox/runners/jest/globalSetup',
   globalTeardown: 'detox/runners/jest/globalTeardown',
