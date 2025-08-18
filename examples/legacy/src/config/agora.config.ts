47720721f0663a4583ab41846f96014380e4945c
gxz <guoxianzhe@agora.io>
2025-08-18 15:36:39 +0800

    chore: optimize

--- examples/legacy/src/config/agora.config.ts
+++ examples/legacy/src/config/agora.config.ts
@@ -1,14 +1,13 @@
-let localAppId = '';
+import { appId } from './appID';
 try {
-  localAppId = require('./appID').default;
-  console.log('appID', localAppId);
+  console.log('appID', appId);
 } catch (error) {
   console.warn(error);
 }
 
 const config = {
   // Get your own App ID at https://dashboard.agora.io/
-  appId: localAppId,
+  appId: appId,
   // Please refer to https://docs.agora.io/en/Agora%20Platform/token
   token: '',
   channelId: 'testdcg',
