c4ee60be1a4c5107bbae729cea1b7ec334a0b43e
gxz <guoxianzhe@agora.io>
2025-08-18 16:49:00 +0800

    chore: optimize

--- examples/expo/app/_layout.tsx
+++ examples/expo/app/_layout.tsx
@@ -3,6 +3,7 @@ import { Stack } from 'expo-router';
 import React, { useState } from 'react';
 
 import { Keyboard, StyleSheet } from 'react-native';
+import { AgoraPipState } from 'react-native-agora';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
@@ -10,7 +11,7 @@ import { LogSink } from '../src/components/LogSink';
 
 import { AgoraText } from '../src/components/ui';
 
-import { PipStateProvider } from '../src/context/pip';
+import { PipStateConsumer, PipStateProvider } from '../src/context/pip';
 
 import Advanced from './examples/advanced';
 import Basic from './examples/basic';
@@ -35,38 +36,57 @@ export default function RootLayout() {
   return (
     <GestureHandlerRootView>
       <PipStateProvider>
-        <SafeAreaView
-          style={styles.container}
-          onStartShouldSetResponder={(_) => {
-            Keyboard.dismiss();
-            return false;
-          }}
-        >
-          <Stack>
-            <Stack.Screen name="index" options={{ title: 'API Example' }} />
-            {Basic.data.map((item) => (
-              <Stack.Screen
-                key={item.name}
-                name={`examples/basic/${item.name}/${item.name}`}
-                options={{ title: item.name, headerRight: () => <Header /> }}
-              />
-            ))}
-            {Advanced.data.map((item) => (
-              <Stack.Screen
-                key={item.name}
-                name={`examples/advanced/${item.name}/${item.name}`}
-                options={{ title: item.name, headerRight: () => <Header /> }}
-              />
-            ))}
-            {Hooks.data.map((item) => (
-              <Stack.Screen
-                key={item.name}
-                name={`examples/hook/${item.name}/${item.name}`}
-                options={{ title: item.name, headerRight: () => <Header /> }}
-              />
-            ))}
-          </Stack>
-        </SafeAreaView>
+        <PipStateConsumer>
+          {(context) => (
+            <SafeAreaView
+              style={styles.container}
+              onStartShouldSetResponder={(_) => {
+                Keyboard.dismiss();
+                return false;
+              }}
+            >
+              <Stack>
+                <Stack.Screen name="index" options={{ title: 'API Example' }} />
+                {Basic.data.map((item) => (
+                  <Stack.Screen
+                    key={item.name}
+                    name={`examples/basic/${item.name}/${item.name}`}
+                    options={{
+                      title: item.name,
+                      headerRight: () => <Header />,
+                      headerShown:
+                        context.pipState !== AgoraPipState.pipStateStarted,
+                    }}
+                  />
+                ))}
+                {Advanced.data.map((item) => (
+                  <Stack.Screen
+                    key={item.name}
+                    name={`examples/advanced/${item.name}/${item.name}`}
+                    options={{
+                      title: item.name,
+                      headerRight: () => <Header />,
+                      headerShown:
+                        context.pipState !== AgoraPipState.pipStateStarted,
+                    }}
+                  />
+                ))}
+                {Hooks.data.map((item) => (
+                  <Stack.Screen
+                    key={item.name}
+                    name={`examples/hook/${item.name}/${item.name}`}
+                    options={{
+                      title: item.name,
+                      headerRight: () => <Header />,
+                      headerShown:
+                        context.pipState !== AgoraPipState.pipStateStarted,
+                    }}
+                  />
+                ))}
+              </Stack>
+            </SafeAreaView>
+          )}
+        </PipStateConsumer>
       </PipStateProvider>
     </GestureHandlerRootView>
   );
