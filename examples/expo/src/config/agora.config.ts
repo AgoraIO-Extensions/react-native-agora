065618d36592f0860ce88918a679a808ff17bd24
gxz <guoxianzhe@agora.io>
2025-08-19 17:41:59 +0800

    chore: optimize

--- examples/expo/src/config/agora.config.ts
+++ examples/expo/src/config/agora.config.ts
@@ -1,13 +1,14 @@
-import { appId } from './appID';
+let localAppId = '';
 try {
-  console.log('appID', appId);
+  localAppId = require('./appID');
+  console.log('appID', localAppId);
 } catch (error) {
   console.warn(error);
 }
 
 const config = {
   // Get your own App ID at https://dashboard.agora.io/
-  appId: appId,
+  appId: localAppId,
   // Please refer to https://docs.agora.io/en/Agora%20Platform/token
   token: '',
   channelId: 'testdcg',
