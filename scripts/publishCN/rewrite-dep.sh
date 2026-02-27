225a9b1d1f6a4748daecdfd527e9a7a811b2f345
gxz <guoxianzhe@agora.io>
2026-02-27 11:28:09 +0800

    chore: optimize

--- scripts/publishCN/rewrite-dep.sh
+++ scripts/publishCN/rewrite-dep.sh
@@ -7,7 +7,3 @@ PROJECT_ROOT=$(realpath ${MY_PATH}/../..)
 change_file=${PROJECT_ROOT}/package.json
 sed "s/${old_package_name}/${new_package_name}/g" ${change_file} > tmp && mv tmp ${change_file}
 echo "${change_file} rewritten successfully"
-
-change_file2=${PROJECT_ROOT}/yarn.lock
-sed "s/${old_package_name}/${new_package_name}/g" ${change_file2} > tmp && mv tmp ${change_file2}
-echo "${change_file2} rewritten successfully"
